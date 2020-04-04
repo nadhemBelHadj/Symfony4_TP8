497e6481dc1f283f3a0e7cf50218ee870b3ad635
Nadhem Bel Hadj <39857478+nadhemBelHadj@users.noreply.github.com>
2020-04-04 14:48:55 +0100

    Add files via upload

---
+++ src/Entity/CategorySearch.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+class CategorySearch
+{
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category", inversedBy="articles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+
+
+}
